895b1d4b6adbcd08ac86e23f47768eca3cefc261
scattergame <90742519+scattergame@users.noreply.github.com>
2023-04-17 13:21:02 -0400

    add a couple of menu items

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,23 @@
 import { MenuItem } from "./MenuItem"
 import { AiFillHome }  from 'react-icons/ai'
+import { BsInfoCircleFill }  from 'react-icons/Bs'
+import Link from "next/link"
 
 export const Header = () => {
     return (
-        <div>
-            <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+        <div className="flex justify-between mx-2 max-w-6xl sm:max-auto items-center py-6">
+            <div className="flex">
+                <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+                <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill}/>
+            </div>
+            <div>
+                <Link href='/'>
+                    <h2 className="text-2xl">
+                        <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
+                        <span className="text-xl hidden sm:inline">Clone</span>
+                    </h2>
+                </Link>
+            </div>
         </div>
     )
 }
