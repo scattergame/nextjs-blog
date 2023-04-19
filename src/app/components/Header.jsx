25d945761877494a103bb9cbd3b5f7032de25dbf
scattergame <90742519+scattergame@users.noreply.github.com>
2023-04-19 09:24:10 -0400

    formatted

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,25 +1,27 @@
-import { MenuItem } from "./menuitem"
-import { AiFillHome }  from 'react-icons/ai'
-import { BsInfoCircleFill }  from 'react-icons/bs'
-import Link from "next/link"
-import { DarkModeSwitch } from "./darkModeswitch"
+import { MenuItem } from "./menuitem";
+import { AiFillHome } from "react-icons/ai";
+import { BsInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import { DarkModeSwitch } from "./darkModeswitch";
 
 export const Header = () => {
-    return (
-        <div className="flex justify-between mx-2 max-w-6xl sm:max-auto items-center py-6 select-none">
-            <div className="flex">
-                <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
-                <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill}/>
-            </div>
-            <div className="flex space-x-6 items-center">
-                <DarkModeSwitch/>
-                <Link href='/'>
-                    <h2 className="text-2xl">
-                        <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
-                        <span className="text-xl hidden sm:inline">Clone</span>
-                    </h2>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:max-auto items-center py-6 select-none">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill} />
+      </div>
+      <div className="flex space-x-6 items-center">
+        <DarkModeSwitch />
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { NavbarItem } from './navbaritem'
+import React from "react";
+import { NavbarItem } from "./navbaritem";
 
 export const Navbar = () => {
   return (
-    <div className='flex justify-center dark:bg-gray-500 bg-amber-100 text-sm lg:text-md p-4'>
-        <NavbarItem title="Trending" param="fetchTrending" />   
-        <NavbarItem title="Top Rated" param="fetchTopRated" />  
+    <div className="flex justify-center dark:bg-gray-500 bg-amber-100 text-sm lg:text-md p-4">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
-  )
-}
+  );
+};
